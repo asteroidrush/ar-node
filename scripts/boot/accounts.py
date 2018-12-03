1e690e2e2e5edc8be66929053dea4c9e585c5bb2
Ilya Shmelev <ishmelev23@gmail.com>
2018-12-03 16:39:42 +0300

    ARB-75: Added contracts setup based on boot_config

--- scripts/boot/accounts.py
+++ scripts/boot/accounts.py
@@ -9,16 +9,19 @@ class AccountManager:
         'kb': 1024
     }
 
-    def __init__(self, cleos, contracts_manager, tokens_info):
+    def __init__(self, cleos, contracts_manager, tokens_info, temp_public_key):
         self.cleos = cleos
         self.contracts_manager = contracts_manager
         self.tokens_info = tokens_info
 
+        # necessary for centralized management during boot sequence
+        self.temp_public_key = temp_public_key
+
     def create(self, name, pub):
         self.cleos.run('create account eosio %s %s' % (name, pub))
 
-    def create_staked(self, name, pub, tokens, ram="default", net="default", cpu="default", contract_host=False):
-        self.cleos.run('system newaccount eosio %s %s -p eosio@createaccnt' % (name, pub) )
+    def create_staked(self, name, tokens, ram="default", net="default", cpu="default", contract_host=False):
+        self.cleos.run('system newaccount eosio %s %s -p eosio@createaccnt' % (name, self.temp_public_key) )
         for token_name, amount in tokens.items():
             token_data = self.tokens_info[token_name]
             self.cleos.run('transfer eosio %s "%s"' % (name, Wallet.int_to_currency(amount, token_data['shortcut'], token_data['precision'])))
@@ -55,14 +58,15 @@ class AccountsManager:
         government_account
     ] + ContractsManager.system_contracts
 
-    def __init__(self, wallet, cleos, contracts_manager, tokens_info):
+    def __init__(self, wallet, cleos, contracts_manager, tokens_info, contracthost_temp_key):
         self.wallet = wallet
-        self.account_manager = AccountManager(cleos, contracts_manager, tokens_info)
+        self.account_manager = AccountManager(cleos, contracts_manager, tokens_info, contracthost_temp_key)
 
     def create_system_account(self, name):
         keys = self.wallet.create_keys()
         self.wallet.import_key(keys['pvt'])
         self.account_manager.create(name, keys['pub'])
+        self.account_manager.contracts_manager.unlock_contract_uploading(name)
 
     def create_system_accounts(self):
         for account_name in self.system_accounts:
@@ -70,5 +74,5 @@ class AccountsManager:
 
     def create_accounts(self, accounts):
         for account in accounts:
-            self.account_manager.create_staked(account['name'], account['pub'], account['tokens'],
+            self.account_manager.create_staked(account['name'], account['tokens'],
                                                account['ram'], account['net'], account['cpu'], account['contract_host'])
--- scripts/boot/contracts.py
+++ scripts/boot/contracts.py
@@ -1,3 +1,6 @@
+from time import sleep
+
+
 class ContractsManager:
     system_contracts = ["eosio.token", "eosio.msig"]
 
@@ -6,7 +9,6 @@ class ContractsManager:
         self.dir = dir
 
     def install(self, account_name, contract_name):
-        self.unlock_contract_uploading(account_name)
         self.cleos.run(('set contract %s ' + self.dir + '%s/') % (account_name, contract_name))
 
     def unlock_contract_uploading(self, account_name):
@@ -19,3 +21,7 @@ class ContractsManager:
     def install_system_contract(self):
         self.install('eosio', 'eosio.system')
         self.cleos.run('push action eosio setpriv \'["eosio.msig", 1]\' -p eosio@active -x 1000')
+
+    def install_contracts(self, contracts):
+        for contract_data in contracts:
+            self.install(contract_data['account'], contract_data['name'])
